000fb6a25fdbb25669ae8dad3a53c41eee17daf1
Jean <jeanoosthuizenalt@gmail.com>
2024-12-15 12:05:16 +0200

    Completed day 1

---
+++ answer1-1.py
@@ -0,0 +1,14 @@
+f = open("input1-1.txt","r")
+arr1 = []
+arr2 = []
+for line in f:
+  vals = line.strip().split("   ")
+  arr1.append(vals[0])
+  arr2.append(vals[1])
+arr1.sort()
+arr2.sort()
+
+tally = 0
+for i in range(0,len(arr1)):
+  tally += abs(int(arr1[i]) - int(arr2[i]))
+print(tally)
---
+++ answer1-2.py
@@ -0,0 +1,17 @@
+f = open("input1-1.txt","r")
+arr1 = []
+arr2 = []
+for line in f:
+  vals = line.strip().split("   ")
+  arr1.append(int(vals[0]))
+  arr2.append(int(vals[1]))
+
+similarity = 0
+for i in range(0,len(arr1)):
+  matchCount = 0
+  searchVal = arr1[i]
+  for j in range(0,len(arr2)):
+    if(arr2[j] == searchVal):
+      matchCount += 1
+  similarity += searchVal * matchCount
+print(similarity)
